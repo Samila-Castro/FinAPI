7ffda7103e66b5e971cf5255817a36dbba8ae420
Samila Castro <samilav1997@gmail.com>
2021-10-02 20:25:24 -0300

    up

--- src/index.js
+++ src/index.js
@@ -5,19 +5,18 @@ const app = express();
 
 app.use(express.json());
 
-const costomers = [];
+const customers = [];
 
 app.post("/account", (request, response) => {
     const { name, cpf } = request.body;
 
-    const customerAlreadyExists = costomers.some(
-        (customer) => customerAlreadyExists.cpf === cpf
-        );
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+  
     if(customerAlreadyExists) {
-        return response.status(400).json( { error: "Customer already exists!"});
+        return response.status(400).json( { error: "Customer already exists!" });
     }
 
-    costumers.push({
+    customers.push({
         cpf,
         name,
         id: uuidv4(),
