2c2f1436ee86f11bd611eb138569dd695af31fa3
Samila Castro <samilav1997@gmail.com>
2021-10-04 00:02:10 -0300

    Adiciona middleware de verificação do cpf

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next){
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer => customer.cpf === cpf ));
+
+    if(!customer) {
+        return response.status(400).json( { error: "Customer not found!"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
     const { name, cpf } = request.body;
 
@@ -26,15 +40,9 @@ app.post("/account", (request, response) => {
 
 });
 
-app.get("/statement/:cpf", (request, response) => {
-    const { cpf } = request.params;
-
-    const customer = customers.find((customer => customer.cpf === cpf ));
-
-    if(!customer) {
-        return response.status(400).json( { error: "Customer not found!"});
-    }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    
     return response.json(customer.statement);
 });
 
